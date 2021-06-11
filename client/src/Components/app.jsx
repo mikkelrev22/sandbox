a313974efc39327d59249fbced0b16b55de1fa49
mike_thompson <mike.thompson@ibm.com>
2021-06-11 11:13:01 -0700

    webpack bundle created

---
+++ client/src/Components/app.jsx
@@ -0,0 +1,12 @@
+import React,  {useState, useEffect} from 'react'
+
+const App = () => {
+console.log('test of react portion')
+
+return (
+    <div className="app">
+        testing react portion
+    </div>
+)
+}
+export default App
