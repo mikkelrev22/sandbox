edb8d7d09f6ba4148910508422f386a1c6fafe74
mike_thompson <mike.thompson@ibm.com>
2021-06-11 10:17:41 -0700

    completeing error handling in server

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, '/../dist')))
 
+app.use((req, res, next)=>{
+    res.sendStatus(400)
+  })
+  
+app.use((err, req, res, next)=>{
+    console.log(err)
+    res.sendStatus(500).send(err.stack)
+  })
+
 app.listen(PORT, () => {
     console.log(`server is now listening on Port ${PORT}. Nice to see you today!`)
 })
