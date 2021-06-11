2ff427072ab5ebe8319e9fee559cb2fb275bbb5a
mike_thompson <mike.thompson@ibm.com>
2021-06-11 11:17:06 -0700

    created test dir

--- client/src/Components/app.jsx
+++ client/src/Components/app.jsx
@@ -1,8 +1,9 @@
 import React,  {useState, useEffect} from 'react'
 
 const App = () => {
-console.log('test of react portion')
 
+
+    
 return (
     <div className="app">
         testing react portion
