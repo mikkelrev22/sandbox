ff4a85c12e46af51678d9cec07759f2688274d95
mike_thompson <mike.thompson@ibm.com>
2021-06-14 11:33:01 -0700

    created basic components

--- client/src/Components/app.jsx
+++ client/src/Components/app.jsx
@@ -1,13 +1,41 @@
 import React,  {useState, useEffect} from 'react'
 
-const App = () => {
-
-
-    
-return (
-    <div className="app">
-        testing react portion
+//XML UI of client
+const ClientComponent = ({clientInfo}) => {
+    return <div>
+        this is the client:
+        {clientInfo}
     </div>
-)
+}
+
+//XML UI of order
+const OrderComponent = ({orderInfo}) => {
+    return <div>
+        this is the order:
+        {orderInfo}
+    </div>
+}
+
+const App = () => {
+    const [clientInfo, setClientInfo] = useState([])
+    const [orderInfo, setOrderInfo] = useState([])
+
+//useEffect on every occurrence of what? API call/response? How to seperate multiple incoming orders/calls?
+
+//Dummy API call/response
+    const getData = () => {
+        
+    }
+
+    return (
+        <div className="app">
+            <div>
+            <ClientComponent clientInfo={clientInfo}/>
+            </div>
+            <div>
+            <OrderComponent orderInfo={orderInfo}/>
+            </div>
+        </div>
+    )
 }
 export default App
